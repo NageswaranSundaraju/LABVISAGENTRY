2c7b88766bea7f196f4a6da2a20b6ed55973be21
NageswaranSundaraju <nageswaran1610@gmail.com>
2024-05-22 11:18:13 +0800

    FEAT: >ADD HEADER >DetailFrame >Name Label

--- DBPS/MainUI.py
+++ DBPS/MainUI.py
@@ -1,1 +1,27 @@
+from tkinter import *
+# import tkinter as tk
 
+from tkinter import Toplevel
+
+
+def main():
+    global root
+    root = Tk()
+    root.title("LVE ADMIN")
+    root.geometry("955x500")
+
+    HeaderTitle = Label(root, text='Lab Visage Entry Admin', font=("Arial", 40, "bold"))
+    HeaderTitle.pack()
+
+    detailFrame = Frame(root, bg='White', height=310, width=430)
+    detailFrame.pack(side = LEFT, padx = 50)
+
+
+    nameLabel = Label(detailFrame, text='Name')
+    nameLabel.pack()
+
+    # Run the Tkinter event loop
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
