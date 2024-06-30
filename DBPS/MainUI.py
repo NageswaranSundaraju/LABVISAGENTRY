25b808869233a38753829f17d9707ae75633699c
NageswaranSundaraju <nageswaran1610@gmail.com>
2024-07-01 03:26:20 +0800

    FEAT: CAN CHECK CONNECTION, LOGGING

--- DBPS/MainUI.py
+++ DBPS/MainUI.py
@@ -4,117 +4,172 @@ import mysql.connector
 import DeviceStatus
 from CaptureFace import capture_images
 import TkinterDisplayLog
-from tkinter import *
 import train_model
+import showlect
+import logging
+import scp
 
-def main():
-    global root
-    root = tk.Tk()
-    root.title("LVE ADMIN")
-    root.geometry("955x500")
 
-    HeaderTitle = tk.Label(root, text='Lab Visage Entry Admin', font=("Arial", 40, "bold"))
-    HeaderTitle.pack(pady=10)
 
-    # TextConfig
-    NORMALTXT = ("Arial", 15)
-    TITLETXT = ("Arial", 15, 'bold')
+class LVEAdminApp:
 
-    # Detail Section
-    Dframe = tk.Frame(root, height=250, width=250)
-    Dframe.pack(side=LEFT, anchor='w', padx=40, ipadx=15)
+    def __init__(self, root):
 
-    NameLabel = tk.Label(Dframe, text='NAME', font=(NORMALTXT))
-    NameLabel.grid(row=0, column=0, pady=10)
-    NOICLabel = tk.Label(Dframe, text='NO IC', font=(NORMALTXT))
-    NOICLabel.grid(row=1, column=0, pady=10)
-    NOTELLabel = tk.Label(Dframe, text='NO TEL', font=(NORMALTXT))
-    NOTELLabel.grid(row=2, column=0, pady=10)
-    NicknameLabel = tk.Label(Dframe, text='NICKNAME', font=(NORMALTXT))
-    NicknameLabel.grid(row=3, column=0, pady=10)
+        self.root = root
+        self.root.title("LVE ADMIN")
+        self.root.geometry("850x500")
 
-    global NameEntry, NOICEntry, NOTELEntry, NicknameEntry  # Declare as global variables to access them in other functions
-    NameEntry = tk.Entry(Dframe)
-    NameEntry.grid(row=0, column=1)
-    NameEntry.config(bg='White', fg='Black')
-    NOICEntry = tk.Entry(Dframe)
-    NOICEntry.grid(row=1, column=1)
-    NOICEntry.config(bg='White', fg='Black')
-    NOTELEntry = tk.Entry(Dframe)
-    NOTELEntry.grid(row=2, column=1)
-    NOTELEntry.config(bg='White', fg='Black')
-    NicknameEntry = tk.Entry(Dframe)
-    NicknameEntry.grid(row=3, column=1)
-    NicknameEntry.config(bg='White', fg='Black')
+        # Fonts
+        self.TITLE_FONT = ("Arial", 40, "bold")
+        self.NORMAL_FONT = ("Arial", 15)
+        self.BUTTON_FONT = ("Arial", 12)
 
-    def open_camera():
-        username = NicknameEntry.get()
-        if username.strip():  # Check if the username is not empty
+        # Button size
+        self.BUTTON_WIDTH = 20
+        self.BUTTON_HEIGHT = 2
+
+        self.create_widgets()
+
+        self.check_db_connection()  # Check DB connection initially
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+        self.logger = logging.getLogger(__name__)
+        file_handler = logging.FileHandler('app.log')
+        formatter = logging.Formatter('%(asctime)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+
+
+    def create_widgets(self):
+        # Header
+        header_frame = tk.Frame(self.root, width=955, height=100)
+        header_frame.pack(pady=(10, 20))
+
+        header_label = tk.Label(header_frame, text="Lab Visage Entry Admin", font=self.TITLE_FONT)
+        header_label.pack(pady=10)
+
+        # Detail Section
+        detail_frame = tk.Frame(self.root, width=400, height=300)
+        detail_frame.pack(side=tk.LEFT, anchor='nw', padx=20, pady=10)
+
+        name_label = tk.Label(detail_frame, text='NAME', font=self.NORMAL_FONT)
+        name_label.grid(row=0, column=0, pady=10, padx=10, sticky='w')
+        noic_label = tk.Label(detail_frame, text='NO IC', font=self.NORMAL_FONT)
+        noic_label.grid(row=1, column=0, pady=10, padx=10, sticky='w')
+        notel_label = tk.Label(detail_frame, text='NO TEL', font=self.NORMAL_FONT)
+        notel_label.grid(row=2, column=0, pady=10, padx=10, sticky='w')
+
+        self.name_entry = tk.Entry(detail_frame, font=self.NORMAL_FONT)
+        self.name_entry.grid(row=0, column=1, padx=10, pady=10)
+        self.noic_entry = tk.Entry(detail_frame, font=self.NORMAL_FONT)
+        self.noic_entry.grid(row=1, column=1, padx=10, pady=10)
+        self.notel_entry = tk.Entry(detail_frame, font=self.NORMAL_FONT)
+        self.notel_entry.grid(row=2, column=1, padx=10, pady=10)
+
+        open_cam_btn = tk.Button(detail_frame, text='OPEN CAMERA', command=self.open_camera, font=self.BUTTON_FONT,
+                                 width=self.BUTTON_WIDTH, height=self.BUTTON_HEIGHT)
+        open_cam_btn.grid(row=4, column=1, pady=20, columnspan=2, padx=10)
+
+        enroll_btn = tk.Button(detail_frame, text='Enroll Face', command=self.enroll_face, font=self.BUTTON_FONT,
+                               width=self.BUTTON_WIDTH, height=self.BUTTON_HEIGHT)
+        enroll_btn.grid(row=1, column=2, padx=30, pady=10, ipadx=10)
+
+        clear_btn = tk.Button(detail_frame, text='Clear', command=self.clear_entries, font=self.BUTTON_FONT,
+                              width=self.BUTTON_WIDTH, height=self.BUTTON_HEIGHT)
+        clear_btn.grid(row=2, column=2, padx=30, pady=10, ipadx=20)
+
+        # Setting frame
+        self.setting_frame = tk.Frame(self.root, width=200, height=300)
+        self.setting_frame.pack(side=tk.LEFT, anchor='nw', padx=20, pady=10)
+
+        setting_label = tk.Label(self.setting_frame, text='Settings', font=self.TITLE_FONT)
+        setting_label.pack(pady=10)
+
+
+
+        system_log_btn = tk.Button(self.setting_frame, text='System Logs', command=TkinterDisplayLog.choose_log_file,
+                                   font=self.BUTTON_FONT, width=self.BUTTON_WIDTH, height=self.BUTTON_HEIGHT)
+        system_log_btn.pack(pady=10, ipadx=10)
+
+        update_face_btn = tk.Button(self.setting_frame, text='Refresh', command=train_model.create_gui,
+                                    font=self.BUTTON_FONT, width=self.BUTTON_WIDTH, height=self.BUTTON_HEIGHT)
+        update_face_btn.pack(pady=10, ipadx=10)
+
+        device_status_btn = tk.Button(self.setting_frame, text='Device Status', command=DeviceStatus.create_gui,
+                                      font=self.BUTTON_FONT, width=self.BUTTON_WIDTH, height=self.BUTTON_HEIGHT)
+        device_status_btn.pack(pady=10, ipadx=10)
+
+        list_names_btn = tk.Button(self.setting_frame, text='List Names', command=showlect.create_gui,
+                                   font=self.BUTTON_FONT, width=self.BUTTON_WIDTH, height=self.BUTTON_HEIGHT)
+        list_names_btn.pack(pady=10, ipadx=10)
+
+        self.db_status_label = tk.Label(self.setting_frame, text='Database: Disconnected', font=self.NORMAL_FONT,
+                                        fg='red')
+        self.db_status_label.pack(pady=10)
+
+    def check_db_connection(self):
+        try:
+            connection = mysql.connector.connect(
+                user='NAGES',
+                password='ROOT',
+                host='192.168.0.254',
+                database='lvedb'
+            )
+            if connection.is_connected():
+                self.db_status_label.config(text='Database: Connected', fg='green')
+                connection.close()
+            else:
+                self.db_status_label.config(text='Database: Disconnected', fg='red')
+        except mysql.connector.Error as err:
+            self.db_status_label.config(text='Database: Disconnected', fg='red')
+            logging.error(f"DATABASE ERROR {err}")
+
+    def open_camera(self):
+        username = self.noic_entry.get()
+        if username.strip():
             capture_images(username)
         else:
-            tk.messagebox.showwarning("Warning", "Please enter a name before opening the camera.")
+            messagebox.showwarning("Warning", "Please enter a name before opening the camera.")
 
-    def clear_entries():
-        NameEntry.delete(0, END)
-        NOICEntry.delete(0, END)
-        NOTELEntry.delete(0, END)
-        NicknameEntry.delete(0, END)
+    def clear_entries(self):
+        self.name_entry.delete(0, tk.END)
+        self.noic_entry.delete(0, tk.END)
+        self.notel_entry.delete(0, tk.END)
 
-    def enroll_face():
-        name = NameEntry.get()
-        noic = NOICEntry.get()
-        notel = NOTELEntry.get()
-        nickname = NicknameEntry.get()
+    def enroll_face(self):
+        name = self.name_entry.get()
+        noic = self.noic_entry.get()
+        notel = self.notel_entry.get()
 
-        if name.strip() and noic.strip() and notel.strip() and nickname.strip():
-            summary = f"Name: {name}\nNo IC: {noic}\nNo Tel: {notel}\nNickname: {nickname}"
-            confirm = messagebox.askokcancel("Confirm Data", f"Please confirm the following data will be added to the database:\n\n{summary}")
+        if name.strip() and noic.strip() and notel.strip():
+            summary = f"Name: {name}\nNo IC: {noic}\nNo Tel: {notel}\n"
+            confirm = messagebox.askokcancel("Confirm Data",
+                                             f"Please confirm the following data will be added to the database:\n\n"
+                                             f"{summary}")
             if confirm:
                 try:
                     connection = mysql.connector.connect(
-                        user='Nages',
-                        password='admin',
-                        host='192.168.1.101',
+                        user='NAGES',
+                        password='ROOT',
+                        host='192.168.0.254',
                         database='lvedb'
                     )
                     cursor = connection.cursor()
-                    insert_query = "INSERT INTO lect (noic, name, notel, nickname) VALUES (%s, %s, %s, %s)"
-                    cursor.execute(insert_query, (noic, name, notel, nickname))
+                    insert_query = "INSERT INTO lect (noic, name, notel) VALUES (%s, %s, %s)"
+                    cursor.execute(insert_query, (noic, name, notel))
                     connection.commit()
                     cursor.close()
                     connection.close()
                     messagebox.showinfo("Success", "Data inserted successfully!")
-                    clear_entries()
+                    self.logger.info(f"Data Added Name: {name} NoIC: {noic}")
+                    self.clear_entries()
                 except mysql.connector.Error as err:
                     messagebox.showerror("Error", f"Error: {err}")
+                    logging.error(f"DATABASE ERROR {err}")
         else:
             messagebox.showwarning("Warning", "Please fill in all fields.")
 
-    OPENCAMBtn = tk.Button(Dframe, text='OPEN CAMERA', command=open_camera)  # Link the button to the open_camera function
-    OPENCAMBtn.grid(row=4, column=1, pady=20, columnspan=1)
-
-    EnrollBtn = tk.Button(Dframe, text='Enroll Face', command=enroll_face)
-    EnrollBtn.grid(row=1, column=2, padx=50, ipadx=5)
-
-    ClearBtn = tk.Button(Dframe, text='Clear', command=clear_entries)
-    ClearBtn.grid(row=2, column=2, padx=50, ipadx=25)
-
-    # Setting frame
-    settingFrame = tk.Frame(root, bg='dark grey', height=150, width=250)
-    settingFrame.pack(side=LEFT, anchor='w', padx=50, ipady=40, ipadx=80)
-
-    settinglabel = tk.Label(settingFrame, text='Setting', font=TITLETXT)
-    settinglabel.pack(anchor='w', pady=5, padx=5)
-
-    SystemLogBtn = tk.Button(settingFrame, text='System Logs', activebackground='dark grey', command=TkinterDisplayLog.log_file_viewer)
-    SystemLogBtn.pack(ipady=10, pady=10)
-    UpdateFaceBtn = tk.Button(settingFrame, text='Refresh', activebackground='dark grey', command=train_model.train)
-    UpdateFaceBtn.pack(ipady=10, pady=10)
-    StatusBtn = tk.Button(settingFrame, text='Device Status', activebackground='dark grey', command=DeviceStatus.create_gui)
-    StatusBtn.pack(ipady=10, pady=10)
-
-    # Run the Tkinter event loop
-    root.mainloop()
 
 if __name__ == '__main__':
-    main()
+    root = tk.Tk()
+    app = LVEAdminApp(root)
+    root.mainloop()
--- temp/FACERECOGNITION/facial_req.py
+++ temp/FACERECOGNITION/facial_req.py
@@ -5,43 +5,117 @@ import pickle
 import time
 import cv2
 import threading
+import dlib
+from imutils import face_utils  # Import face_utils for shape_to_np
+from scipy.spatial import distance
+import queue
 
 # Initialize variables
 current_name = "unknown"
-encodings_file = "../DBPS/encodings.pickle"
+encodings_file = "encodings.pickle"
 data = pickle.loads(open(encodings_file, "rb").read())
-vs = VideoStream(src=0, framerate=30).start()
+vs = VideoStream(src=0, resolution=(640, 480)).start()  # Adjust resolution if needed
 time.sleep(2.0)
 fps = FPS().start()
 
+# Initialize dlib's face detector and facial landmark predictor
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+# Define indices for left and right eye landmarks
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+# Pre-initialize variables for optimization
+gray = None
+rects = []
+boxes = []
+encodings = []
+
+# Define a function to calculate the eye aspect ratio
+def eye_aspect_ratio(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+# Set thresholds and consecutive frame counters for eye blinking
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 3
+COUNTER = 0
+
+frame_queue = queue.Queue(maxsize=1)
+
+# Function to put frame into the queue
+def put_frame(frame):
+    try:
+        frame_queue.put(frame, block=False)
+    except queue.Full:
+        pass  # Handle queue full condition if needed
+
+# Main thread function to display frames
+def display_frames():
+    while True:
+        try:
+            frame = frame_queue.get(block=True, timeout=0.1)
+            cv2.imshow("Facial Recognition is Running", frame)
+        except queue.Empty:
+            pass  # Handle queue empty condition if needed
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+
 def process_frame(frame):
-    global current_name
-    frame = imutils.resize(frame, width=500)
+    global current_name, COUNTER, gray, rects, boxes, encodings
+
+    frame = imutils.resize(frame, width=500)  # Resize early for efficiency
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    rects = detector(gray, 0)
     boxes = face_recognition.face_locations(frame)
     encodings = face_recognition.face_encodings(frame, boxes)
+
     names = []
 
-    for encoding in encodings:
-        matches = face_recognition.compare_faces(data["encodings"], encoding)
-        name = "Unknown"
-        if True in matches:
-            matched_idxs = [i for (i, b) in enumerate(matches) if b]
-            counts = {}
-            for i in matched_idxs:
-                name = data["names"][i]
-                counts[name] = counts.get(name, 0) + 1
-            name = max(counts, key=counts.get)
-            if current_name != name:
-                current_name = name
-                print(current_name)
-        names.append(name)
+    for rect in rects:
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)  # Convert to NumPy array
+
+        leftEye = shape[lStart:lEnd]  # Extract left eye landmarks
+        rightEye = shape[rStart:rEnd]  # Extract right eye landmarks
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+
+        ear = (leftEAR + rightEAR) / 2.0
+
+        if ear < EYE_AR_THRESH:
+            COUNTER += 1
+        else:
+            if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                current_name = "Liveness detected"
+            COUNTER = 0
+
+        for encoding in encodings:
+            matches = face_recognition.compare_faces(data["encodings"], encoding)
+            name = "Unknown"
+            if True in matches:
+                matched_idxs = [i for (i, b) in enumerate(matches) if b]
+                counts = {}
+                for i in matched_idxs:
+                    name = data["names"][i]
+                    counts[name] = counts.get(name, 0) + 1
+                name = max(counts, key=counts.get)
+                if current_name != name:
+                    current_name = name
+                    print(current_name)
+            names.append(name)
 
     for ((top, right, bottom, left), name) in zip(boxes, names):
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 225), 2)
         y = top - 15 if top - 15 > 15 else top + 15
         cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, .8, (0, 255, 255), 2)
 
-    cv2.imshow("Facial Recognition is Running", frame)
+    put_frame(frame)
 
 def process_video_stream():
     while True:
